ff6b93abd6f43b3286105f541d35406dc41165e8
Abraham Kwok <abrahamkwok628@berkeley.edu>
2024-01-17 18:58:33 -0800

    updated website

--- main.js
+++ main.js
@@ -12,6 +12,13 @@ window.addEventListener("scroll", function(event) {
     if(scroll < 1000) {
         let navOptions = document.getElementsByClassName("nav_options");
         for (let i = 0; navOptions.length; i++)
-        navOptions[i].setAttribute("style", "color: white");
+        navOptions[i].setAttribute("style", "color: #efeba7");
     }
 })
+
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
